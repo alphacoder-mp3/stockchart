e584c6e5fde159006e284ccbc1a0ba7cffd53422
alphacoder-mp3 <shindeakshay223@gmail.com>
2024-06-07 05:58:32 +0530

    Some improvements

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,10 @@
+require('dotenv').config();
 const app = require('./app');
+
 //app.use(dbmodel);
 // set port, listen for requests
 const PORT = process.env.PORT || 8000;
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, { useId, useState } from 'react';
+import React, { useState } from 'react';
 import SymbolInput from './SymbolInput';
 import ChartComponent from './ChartComponent';
 
@@ -6,7 +6,6 @@ const App = () => {
   const [symbol, setSymbol] = useState('');
   const [strikePrice, setStrikePrice] = useState('');
   const [expiryDate, setExpiryDate] = useState('');
-  const uniqId = useId();
 
   const handleSymbolSubmit = (symbol, strikePrice, expiryDate) => {
     setSymbol(symbol);
@@ -14,32 +13,15 @@ const App = () => {
     setExpiryDate(expiryDate);
   };
 
-  const divStyle = {
-    // backgroundImage:
-    //   "url(https://thumbor.forbes.com/thumbor/fit-in/x/https://www.forbes.com/uk/advisor/wp-content/uploads/2023/05/Trading_photo.jpg)",
-    backgroundSize: 'cover',
-    backgroundPosition: 'center',
-    height: '100vh', // Full height of the viewport
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    background: '#000000',
-  };
-  const divStyles = {
-    height: '600px', // Increase the height as needed
-    width: '800px', // Increase the width as needed
-  };
   return (
     <div>
-      <div style={divStyle}>
+      <div className="bg-cover bg-center h-screen flex justify-center items-center bg-neutral-900">
         <SymbolInput onSubmit={handleSymbolSubmit} />
         {symbol && strikePrice && expiryDate && (
           <ChartComponent
-            style={divStyles}
             symbol={symbol}
             strikePrice={strikePrice}
             expiryDate={expiryDate}
-            // key={uniqId}
           />
         )}
       </div>
